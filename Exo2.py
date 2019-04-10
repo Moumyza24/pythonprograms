7920a70ac358c8eae00a0d03ab87a1c3a9343fb6
Mame oumy sarr <45593651+Moumyza24@users.noreply.github.com>
2019-04-10 13:10:43 +0200

    Add files via upload

---
+++ Exo2.py
@@ -0,0 +1,15 @@
+Ecrire un programme python qui permet de verifier si le nombre d utilisateur est pair ou impair
+
+a= input ("nombre pair")
+
+b= input ("nombre impair")
+
+if: ("nombre pair") alors
+
+ print("Laiser passer")
+
+else: "(nombre impair") alors
+
+ print("Bloquer")
+
+
