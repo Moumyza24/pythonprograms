e9431e989db3e31e72ca69f369bef175e1ed4e3e
Mame oumy sarr <45593651+Moumyza24@users.noreply.github.com>
2019-04-10 12:04:18 +0200

    Add files via upload

---
+++ un programme paire impaire.py
@@ -0,0 +1,12 @@
+Ecrire un programme python qui permet de verifier si le nombre d utilisateur est pair ou impair
+
+
+Si (nombre pair) alors
+
+  Laiser passer
+
+Sinon (nombre impair) alors
+
+  Bloquer
+
+Fin si
